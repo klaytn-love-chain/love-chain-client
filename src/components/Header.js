1790079301d0557e2a9487ecac396f55362e536e
DongWook <igerapex@gmail.com>
2022-02-13 00:19:49 +0900

    add logged in status

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,34 @@ import Link from 'next/link'
 import Image from 'next/image';
 import styles from './Header.module.scss';
 
-function Header() {
+function Header({ isLoggedIn }) {
 	return <div className={styles.header}>
 		<Link href="/">
 			<a className={styles.logo}>
 				<Image src="/images/logo.svg" alt="love-chain" width={100} height={50} />
 			</a>
 		</Link>
-		<div>
+		<div className={styles.menu}>
 			<Link href="/item">
 				<a className={styles.menu_item}>
 					Market
 				</a>
 			</Link>
-			<Link href="/login">
-				<a className={styles.menu_item}>
-					Klip 로그인
-				</a>
-			</Link>
+			{
+				isLoggedIn ?
+					(
+						<Link href="/my">
+							<a className={styles.menu_item}>
+								<div className={styles.user}/>
+							</a>
+						</Link>
+					) : (
+						<Link href="/login">
+							<a className={styles.menu_item}>
+								Klip 로그인
+							</a>
+						</Link>
+					)}
 		</div>
 	</div>;
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import styles from './Layout.module.scss';
 function Layout({children, type}) {
 	return (
 		<div className={`${styles.layout} ${type === 'main' && styles.main}`}>
-			<Header />
+			<Header isLoggedIn={false} />  {/* 로그인 상태는 true, 비로그인 상태는 false */ }
 			<div className={styles.container}>
 				{children}
 			</div>
