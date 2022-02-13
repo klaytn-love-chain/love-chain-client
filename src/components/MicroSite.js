a3a9afafd84083b1088712351c73d19aa5e7b639
DongWook <igerapex@gmail.com>
2022-02-13 19:16:58 +0900

    add microsite details

--- src/components/MicroSite.js
+++ src/components/MicroSite.js
@@ -1,22 +1,79 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './MicroSite.module.scss';
-import { LOCK_OWNER_INFO as data } from '../constant'
+import { LOCK_OWNER_INFO as data, DOMAIN_URL, KAKAO_JS_KEY } from '../constant';
 import dayjs from 'dayjs';
+import { Button, Image } from '@chakra-ui/react';
+import { useScript } from "../hooks/useScript";
 
 function MicroSite() {
+	const router = useRouter();
+  const { tokenId } = router.query;
 	const now = dayjs();
 	const coupleDate = dayjs(data.options.date);
+	const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
+	const SHARE_URL = `${DOMAIN_URL}/${tokenId}`;
+
+	useEffect(() => {
+		if (status === "ready" && window.Kakao) {
+				// 중복 initialization 방지
+				if (!window.Kakao.isInitialized()) {
+						// 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+					window.Kakao.init(KAKAO_JS_KEY);
+					window.Kakao.Link.sendDefault({
+						objectType: "feed",
+							content: {
+								title: '영원한 사랑의 약속, 러브체인',
+								description: "내용!",
+								imageUrl: "https://love-chain.vercel.app/images/sample-lock.png",
+								link: {
+									mobileWebUrl: SHARE_URL,
+									androidExecParams: "test",
+								},
+							},
+							buttons: [
+								{
+									title: "웹으로 이동",
+									link: {
+										mobileWebUrl: SHARE_URL,
+									},
+								},
+							],
+					});
+				}
+		}
+	}, [SHARE_URL, status]);
+
+	const handleKakaoButton = () => {
+		window.Kakao.Link.sendScrap({
+				requestUrl: SHARE_URL,
+		});
+};
 
 	return (
 		<div className={styles.container}>
 			<img className={styles.lock_image} src="/images/sample-lock.png" alt="lock" />
 			<div className={styles.profile}>
-				<img className={styles.profile_one} src={data.profileImage.oneImage} alt="oneImage" />
-				<img className={styles.profile_two} src={data.profileImage.twoImage} alt="oneImage" />
+				<img className={styles.profile_one} src={data.profileImage.oneImage} alt="profile image" />
+				<img className={styles.profile_two} src={data.profileImage.twoImage} alt="profile image" />
 			</div>
 			<div className={styles.couple_name}>현아 💛️ 이던</div>
-			<div className={styles.date}>우리가 만난 지<br/>{`${now.diff(coupleDate, 'day')}일..! 🥰`}</div>
+			<div className={styles.text}>우리가 만난 지<br />{`${now.diff(coupleDate, 'day')}일..! 🥰`}</div>
+			<div className={styles.couple_image}>
+				<img src={data.options.coupleImage} alt="couple image" />
+			</div>
+			<div className={styles.text}>{data.options.oneLine}</div>
+			<div className={styles.util}>
+				<Link href="/">
+					<a>
+						<Image src="/images/logo.svg" alt="love-chain" width={200} height={100} />
+						<p className={styles.slogan}>영원한 사랑의 약속, 러브체인</p>
+					</a>
+				</Link>
+				<Button className={styles.button} colorScheme='yellow' variant='outline' isFullWidth onClick={handleKakaoButton}>카카오로 공유하기</Button>
+			</div>
 		</div>
 	);
 }
--- src/constant/index.js
+++ src/constant/index.js
@@ -1,3 +1,6 @@
+export const DOMAIN_URL = 'https://love-chain.vercel.app';
+export const KAKAO_JS_KEY = '4625c6b714cd4d29948eae59390200c7';
+
 export const LOCK_OWNER_INFO = {
   tokenId: '001',
   lockImage: '/images/sample-lock.png', // 자물쇠 이미지
@@ -8,8 +11,8 @@ export const LOCK_OWNER_INFO = {
   },
   options: {
     date: '2010-10-10',
-    oneLine: '내 사랑 나의 뮤즈💗',
-    coupleImage: 'https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-15/e35/252917471_1844649855738962_6882883648626510129_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=sFY8R6IY5qgAX8jHfnB&edm=ALQROFkBAAAA&ccb=7-4&ig_cache_key=MjcwMDE5NDYyMjIwOTk1MzU1OA%3D%3D.2-ccb7-4&oh=00_AT88BGbJVk8OHfVL6U_T6DGJZOsv3ETyc-Ges2n06ywCng&oe=620F2F1E&_nc_sid=30a2ef', // 커플 이미지
+    oneLine: '내 사랑 나의 뮤즈💗 \n고마워 고맙고 늘 고마워. \n💎💎💎💎💎💎💎',
+    coupleImage: '/images/sample-coupleImage.jpeg', // 커플 이미지
     socialProfile: {
       oneInstagram: 'hyunah_aa',
       twoInstagram: 'hyojong_1994',
