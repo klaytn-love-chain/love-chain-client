d07ca45df93377073b5dff59066bb075bc76beeb
DongWook <igerapex@gmail.com>
2022-02-20 17:23:04 +0900

    change api request

--- pages/item/index.js
+++ pages/item/index.js
@@ -1,63 +1,59 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Layout from '../../src/components/Layout'
-import axios from 'axios';
-import LockCard from '../../src/components/LockCard'
-import FilterToggle from '../../src/components/FilterToggle'
-import { 
-  Button, 
-  HStack, 
-  Text, 
+import Layout from '../../src/components/Layout';
+import { getItems } from '../../src/constant/api';
+import LockCard from '../../src/components/LockCard';
+import FilterToggle from '../../src/components/FilterToggle';
+import {
+  Button,
+  HStack,
+  Text,
   Spinner,
   useDisclosure,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
 
 export default function ItemListPage() {
-
-  const url = 'http://13.125.214.179:3000/api/item?isAvailable=true&price=desc&limit=10&offset=0'
-  const [isLoading, setIsLoading] = useState(false)
-  const [lockData, setlockData] = useState(null)
-
-  // @chakra-ui/react
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = useRef()
+  const [isLoading, setIsLoading] = useState(false);
+  const [lockData, setlockData] = useState(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
 
   const fetchLockData = async () => {
     try {
-      const { data } = await axios.get(url)
-      console.log(data)
-      setlockData(data)
-      setIsLoading(true)
-
-    } catch(err) {
-      console.log(err)
+      setIsLoading(true);
+      const data = await getItems();
+      setlockData(data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
     }
   }
 
   useEffect(() =>{
-    fetchLockData()
+    fetchLockData();
   }, [])
 
   return (
     <>
-      {!isLoading ? (
+      {isLoading ? (
         <Layout>
           <Spinner />
         </Layout>
       ) : (
         <Layout>
-          <HStack 
-            spacing='5%' 
+          <HStack
+            spacing='5%'
             style={{ display: 'inline' }}
           >
-            <Text 
-              size='lg' 
+            <Text
+              size='lg'
               style={{ display: 'inline' }}
             >
-              {lockData.list.length}개의 자물쇠가 검색되었습니다.
+              {lockData && `${lockData.total}개의 자물쇠가 검색되었습니다.`}
             </Text>
-            <Button 
-              colorScheme='purple' 
-              size='sm' 
+            <Button
+              colorScheme='purple'
+              size='sm'
               style={{ display: 'inline' }}
               ref={btnRef}
               onClick={onOpen}
@@ -65,19 +61,21 @@ export default function ItemListPage() {
               필터
             </Button>
             <hr style={{ margin: '1% 10%', border: '0.01em solid gray' }} />
-          </HStack>
-          <LockCard list={lockData.list} />
+            </HStack>
+            {
+              lockData &&  <LockCard list={lockData.list} />
+            }
         </Layout>
       )}
 
       {/* 필터 토글 */}
-      <FilterToggle 
+      <FilterToggle
         btnRef={btnRef}
         isOpen={isOpen}
         onOpen={onOpen}
         onClose={onClose}
       />
-      
+
     </>
   )
 }
---
+++ src/constant/api.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const API_DOMAIN = 'https://hohyeon.site/api';
+
+export const getItem = async (tokenId) => {
+	const { data } = await axios.get(`${API_DOMAIN}/item/${tokenId}`);
+	return data;
+}
+
+export const getItems = async (params) => {
+	const { data } = await axios.get(`${API_DOMAIN}/item`, {
+		params: {
+			isAvailable: true,
+			price: 'desc',
+			limit: 12,
+			offset: 0,
+			...params,
+		}
+	});
+	return data;
+}
+
+export const getItemUserInfo = async (tokenId) => {
+	const { data } = await axios.get(`${API_DOMAIN}/item/${tokenId}/info`);
+	return data;
+}
+
+export const postItemUserInfo = async ({ tokenId, params }) => {
+	const { data } = await axios.post(`${API_DOMAIN}/item/${tokenId}/info`, {
+		params
+	});
+	return data;
+}
+
+export const deleteItemUserInfo = async (tokenId) => {
+	const { data } = await axios.delete(`${API_DOMAIN}/item/${tokenId}`);
+	return data;
+}
+
+export const getUserItems = async (userAddress) => {
+	const { data } = await axios.get(`${API_DOMAIN}/item/${userAddress}/info`);
+	return data;
+}
