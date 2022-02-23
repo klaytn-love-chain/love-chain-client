485121c11b89191a8699dae0f9cfbec2ccaa3ac6
DongWook <igerapex@gmail.com>
2022-02-23 23:21:56 +0900

    fix lint error

--- src/components/NftDetail.js
+++ src/components/NftDetail.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 // import Link from 'next/link';
 import styles from './NftDetail.module.scss';
-import { Image, Text, Button, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react'
+import { Image, Container, Button, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react'
 import QRCode from 'qrcode.react';
 import { buyNft } from '../constant/api';
 
@@ -9,14 +9,14 @@ const DEFAULT_QR_CODE = "DEFAULT";
 function NftDetail({ data }) {
 
     const [qrvalue, setQrvalue] = useState(DEFAULT_QR_CODE);
-    
+
     const onClickBuyButton = (tokenId) => {
         buyNft(tokenId, setQrvalue, (result) => {
             alert(JSON.stringify(result));
         });
         {qrvalue !== "DEFAULT" ? (
             <Container
-              style={{
+            style={{
                 backgroundColor: "white",
                 width: 300,
                 height: 300,
@@ -24,7 +24,7 @@ function NftDetail({ data }) {
               }}
             >
               <QRCode value={qrvalue} size={256} style={{ margin: "auto" }} />
-  
+
               <br />
               <br />
             </Container>
@@ -46,14 +46,14 @@ function NftDetail({ data }) {
             );
         } else {
             return '소유자가 있습니다.';
-        } 
-        
+        }
+
     }
 	return (
         <div className={styles.cardContainer}>
             <div key={data.tokenId} className={styles.card}>
                         <Image src={data.lockImage} />
-                        <br/>                
+                        <br/>
             </div>
             <div key={data.tokenId} className={styles.card}>
             <Table size='sm'>
@@ -69,36 +69,36 @@ function NftDetail({ data }) {
                         <Td>#</Td>
                         <Td>번호</Td>
                         <Td>{data.tokenId}</Td>
-                    </Tr>            
+                    </Tr>
                     <Tr>
                         <Td>$</Td>
                         <Td>가격</Td>
                         <Td>{data.price} Klay</Td>
-                                 
+
                     </Tr>
                     <Tr>
                         <Td>왕관</Td>
                         <Td>커플 이미지</Td>
                         <Td>{available(data.feature.coupleImage)}</Td>
-                                  
+
                     </Tr>
                     <Tr>
                         <Td>날개</Td>
                         <Td>날짜 입력</Td>
                         <Td>{available(data.feature.date)}</Td>
-                                  
+
                     </Tr>
                     <Tr>
                         <Td>클로버</Td>
                         <Td>소셜 프로필</Td>
                         <Td>{available(data.feature.oneLine)}</Td>
-                                 
+
                     </Tr>
                     <Tr>
                         <Td>무늬</Td>
                         <Td>한줄 입력</Td>
                         <Td>{available(data.feature.socialProfile)}</Td>
-                                 
+
                     </Tr>
                 </Tbody>
             </Table>
@@ -107,6 +107,6 @@ function NftDetail({ data }) {
             </div>
         </div>
 	);
-    
+
 }
 export default NftDetail;
