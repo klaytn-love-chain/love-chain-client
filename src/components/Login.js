1ca295f1fd116f14133d2d39641be3e484a43b85
DongWook <igerapex@gmail.com>
2022-02-23 23:04:36 +0900

    change route mypage after login

--- src/components/Login.js
+++ src/components/Login.js
@@ -56,7 +56,7 @@ function Login() {
 
       if (ok) userDispatch({ type: 'LOGIN', userAddress: address, request_key });
       else alert(msg);
-      Router.replace('/');
+      Router.replace('/my');
 
     });
   }, []);
